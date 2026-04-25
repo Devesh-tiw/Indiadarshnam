0d76b790c4fbb08ccdc308cc8deeccdff42cdca5
devesh <devesht373@gmail.com>
2026-04-25 16:09:28 +0530

    here I added range and it,s js and count the temples in era

--- dear.js
+++ dear.js
@@ -1,20 +1,37 @@
 console.log("hello freinds ");
 //Header Area
 //Siderbar Area
- function updateEra(value) {
-        let text = "";
+function updateEra(value) {
+    let year = Number(value);
+    let text = "";
 
-        if (value < 1500) {
-          text = "Ancient Era (" + value + ")";
-        }
-        else if (value >= 1500 && value < 1900) {
-          text = "Medieval Era (" + value + ")";
-        }
-        else {
-          text = "Modern Era (" + value + ")";
-        }
+    if (year <= -1500) {
+        text = "Vedic Era (" + year + ")";
+    } 
+    else if (year <= -200) {
+        text = "Classical Period (" + year + ")";
+    } 
+    else if (year <= 600) {
+        text = "Early Historic (" + year + ")";
+    } 
+    else if (year <= 1200) {
+        text = "Early Medieval (" + year + ")";
+    } 
+    else if (year <= 1526) {
+        text = "Late Medieval (" + year + ")";
+    } 
+    else if (year <= 1857) {
+        text = "Mughal & Maratha (" + year + ")";
+    } 
+    else if (year <= 1947) {
+        text = "Colonial Era (" + year + ")";
+    } 
+    else {
+        text = "Post-Independence (" + year + ")";
+    }
 
-        document.getElementById("eraprint").innerHTML = text;
-      }
+    document.getElementById("eraprint").style.color = "black";
+    document.getElementById("eraprint").innerHTML = text;
+}
 //Middle Area
 //Fotter AREA
